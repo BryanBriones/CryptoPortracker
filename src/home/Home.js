59570213be76af2821d2f3d35f1cf780dcd9f9cb
Bryan Briones <aaronbryan.briones@gmail.com>
2021-05-20 04:43:48 +0800

    fix other warnings

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
